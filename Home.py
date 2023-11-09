d71705131ad374dca300687e4db4d7cebcc61cb4
matteofranza <45538192+matteofranza@users.noreply.github.com>
2023-11-10 00:40:46 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import streamlit.components.v1 as components
 ######################################################################
 ## layout
 ######################################################################
@@ -14,9 +14,20 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
 ######################################################################
-## 
+## Title
 ######################################################################
+st.markdown("""
+    <style>
+    .centered-text {
+        text-align: center;
+    }
+    </style>
+    <h1 class="centered-text">Home</h1>
+    """, unsafe_allow_html=True)
 
-st.markdown("<h1 style='text-align: center'> Home </h1>", unsafe_allow_html=True)
-
-st.write("blabla")
+st.markdown("""
+Eccoci qui! Ne avrete sentito parlare ma l'estate prossima visiterete la Sicilia per partecipare al nostro matrimonio.
+Questa app ha il solo scopo di fornivi una panoramica dell'organizzazione dell'evento per aiutarvi a pianificare il vostro viaggio. Il vero sito web vi verra' svelato con la consegna delle
+partecipazioni (febbraio/marzo 2024).
+Per cominciare potete selezionare l'area di interesse sulla sinistra. Ovviamente sentitve liberi di contattarci privatamente per ogni esigenza.  
+""", unsafe_allow_html=False)
