f1c5f2d5633b92a4814ec9f36db9508d4e3f94e8
matteofranza <45538192+matteofranza@users.noreply.github.com>
2023-11-10 00:41:37 +0100

    Update 3_Wedding day.py

--- pages/3_Wedding day.py
+++ pages/3_Wedding day.py
@@ -13,5 +13,12 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 ######################################################################
 ######################################################################
-st.markdown("<h1 style='text-align: center'> Sistemazioni </h1>", unsafe_allow_html=True)
-st.write("blabla")
+st.markdown("<h1 style='text-align: center'> Wedding day </h1>", unsafe_allow_html=True)
+
+st.markdown("""
+Il rito verra' pronunciato a Villa Fegotto alle ore 18:00 mentre tutto il resto della giornata si prolunghera' fino alle 03:00.
+Per raggiungere il luogo vi verra' fornito un servizio di bus. Gli orari sono ancora da stabilire, come le fermate. Sicuramente cercheremo di venire in contro a tutti programmando fermate strategiche in base ai luoghi dove la gente ha deciso di alloggiare.
+Cio' non significa che vi verremo a prendere casa per casa come quando c'era LVI, ma vi cercheremo di organizzare almeno un punto di incontro a Marina di Ragusa ed una a Ragusa. 
+Putroppo cercate di capire che sara' un giorno complicato per noi, quindi non possiamo accontentare tutti. Se gia' sapete di poter avere delle esigenze diverse diverse dalla soluzione fornita vi prego di comunicarcelo al piu' presto per trovare una soluzione.
+Visto il programma, vi invitiamo a programmare la giornata per essere pronti per le ore 16.00.  
+""", unsafe_allow_html=False)
