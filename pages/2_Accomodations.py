b7d114e7591d430d4642f4c0234ff637a916d905
matteofranza <45538192+matteofranza@users.noreply.github.com>
2023-11-14 00:04:14 +0100

    Update 2_Accomodations.py

--- pages/2_Accomodations.py
+++ pages/2_Accomodations.py
@@ -22,9 +22,8 @@ st.markdown("""
     }
     </style>
     <div class="justify-text">
-    La cerimonia si svolgerà a Villa Fegotto intorno alle ore 18:00/18.30 (Avrete conferma dell’orario quando riceverete la partecipazione). Terminata la cerimonia, mentre noi sogneremo uno spritz scattandoci di malavoglia qualche foto, voi vi potrete godere un buonissimo aperitivo in loco. Al termine della cena inizierà la festa vera e propria: open bar per tutti e discoteca sotto le stelle fino alle 02:30.<br> 
-    Per raggiungere Villa Fegotto, nel caso non abbiate una macchina o non abbiate voglia di guidare (non guidate se pensate di approfittare dell’open bar!!!), vi verrà fornito un servizio navetta. Gli orari sono ancora da stabilire, così come le fermate. Sicuramente cercheremo di venire incontro a tutti, programmando fermate strategiche in base ai luoghi dove deciderete di alloggiare. Ciò non significa che vi verremo a prendere casa per casa come quando c'era LVI, ma cercheremo di organizzare almeno un punto di incontro a Marina di Ragusa ed uno a Ragusa. Lo stesso servizio navetta vi sarà messo a disposizione alla fine della festa, e vi riporterà alla stessa fermata in cui l’avete preso all’andata. <br>
-    PS: un servizio di animazione/baby sitter si prenderà cura dei vostri bambini con età dai 3 ai 9 anni circa, principalmente durante la cena (da definire la possibilità di estendere il servizio anche durante la festa) ;). 
-
+    Visto l’alto numero di amici e familiari che ci raggiungerà da fuori Sicilia, e visto le diverse esigenze individuali, abbiamo deciso di non occuparci direttamente della vostra sistemazione (speriamo sappiate comprendere la situazione ;)). Come già detto, luglio sarà alta stagione, quindi vi consigliamo caldamente di cominciare a cercare e prenotare un posto per la vostra sistemazione. Al momento, ci sono ancora svariate opzioni economiche, soprattutto se vi organizzate in gruppo.<br> 
+    Le piattaforme di prenotazione consigliate sono quelle classiche ([AirBnB](https://www.airbnb.com/), [Booking](https://www.booking.com/index.it.html?aid=397594&label=gog235jc-1DCAEoggI46AdIM1gDaCyIAQGYARS4ARfIAQzYAQPoAQGIAgGoAgO4AsfYtaoGwAIB0gIkMWJkYmJjMDEtNDE2ZC00ZDdmLTlmZDYtY2MxNzg2ZThjZGZj2AIE4AIB&sid=0c9cc786cb4b9dbfe28a52fd0b89a5d3&keep_landing=1&sb_price_type=total&), [campeggio](https://www.campingbaiadelsole.it/))), più i vari Hotel presenti sul posto.<br>
+    Non esitate a contattarci per ogni domanda e dubbio prima di confermare le prenotazioni.
     </div>
     """, unsafe_allow_html=True)
