680d68fb0ebc13dd694e3ad764e525542d66f3a1
matteofranza <45538192+matteofranza@users.noreply.github.com>
2023-11-14 00:03:48 +0100

    Update 3_Wedding day.py

--- pages/3_Wedding day.py
+++ pages/3_Wedding day.py
@@ -16,9 +16,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center'> Wedding day </h1>", unsafe_allow_html=True)
 
 st.markdown("""
-Il rito verra' pronunciato a Villa Fegotto alle ore 18:00 mentre tutto il resto della giornata si prolunghera' fino alle 03:00.
-Per raggiungere il luogo vi verra' fornito un servizio di bus. Gli orari sono ancora da stabilire, come le fermate. Sicuramente cercheremo di venire in contro a tutti programmando fermate strategiche in base ai luoghi dove la gente ha deciso di alloggiare.
-Cio' non significa che vi verremo a prendere casa per casa come quando c'era LVI, ma vi cercheremo di organizzare almeno un punto di incontro a Marina di Ragusa ed una a Ragusa. 
-Putroppo cercate di capire che sara' un giorno complicato per noi, quindi non possiamo accontentare tutti. Se gia' sapete di poter avere delle esigenze diverse diverse dalla soluzione fornita vi prego di comunicarcelo al piu' presto per trovare una soluzione.
-Visto il programma, vi invitiamo a programmare la giornata per essere pronti per le ore 16.00.  
+La cerimonia si svolgerà a Villa Fegotto intorno alle ore 18:00/18.30 (Avrete conferma dell’orario quando riceverete la partecipazione). Terminata la cerimonia, mentre noi sogneremo uno spritz scattandoci di malavoglia qualche foto, voi vi potrete godere un buonissimo aperitivo in loco. Al termine della cena inizierà la festa vera e propria: open bar per tutti e discoteca sotto le stelle fino alle 02:30.<br> 
+Per raggiungere Villa Fegotto, nel caso non abbiate una macchina o non abbiate voglia di guidare (non guidate se pensate di approfittare dell’open bar!!!), vi verrà fornito un servizio navetta. Gli orari sono ancora da stabilire, così come le fermate. Sicuramente cercheremo di venire incontro a tutti, programmando fermate strategiche in base ai luoghi dove deciderete di alloggiare. Ciò non significa che vi verremo a prendere casa per casa come quando c'era LVI, ma cercheremo di organizzare almeno un punto di incontro a Marina di Ragusa ed uno a Ragusa. Lo stesso servizio navetta vi sarà messo a disposizione alla fine della festa, e vi riporterà alla stessa fermata in cui l’avete preso all’andata. <br>
+PS: un servizio di animazione/baby sitter si prenderà cura dei vostri bambini con età dai 3 ai 9 anni circa, principalmente durante la cena (da definire la possibilità di estendere il servizio anche durante la festa) ;). 
 """, unsafe_allow_html=False)
